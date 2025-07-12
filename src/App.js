9b59f8cb7c44a4885e286c87e2f0430096f9adad
danielthanh <danieldoisme@gmail.com>
2025-07-12 15:15:18 +0700

    Add header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <div>
-      <h1>My React App</h1>
+      <Header />
+      <main className="p-8">
+        <h1 className="text-2xl font-bold">Cổng thông tin tuyển sinh</h1>
+      </main>
     </div>
   );
 }
