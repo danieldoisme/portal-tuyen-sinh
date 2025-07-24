7d35582761c99163348ea547cbc309638898290a
danielthanh <danieldoisme@gmail.com>
2025-07-24 21:59:51 +0700

    Add admin login page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import ScrollToTopButton from "./components/ui/ScrollToTopButton";
@@ -11,52 +16,62 @@ import NewsPressPage from "./pages/NewsPressPage";
 import NewsEventsPage from "./pages/NewsEventsPage";
 import NewsDetailPage from "./pages/NewsDetailPage";
 import AdmissionLookupPage from "./pages/AdmissionLookupPage";
+import AdminLoginPage from "./pages/AdminLoginPage";
+import AdminDashboardPage from "./pages/AdminDashboardPage";
+
+const AppLayout = () => {
+  const location = useLocation();
+  const isAdminRoute = location.pathname.startsWith("/admin");
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      {!isAdminRoute && <Header />}
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/thong-bao/tuyen-sinh-dai-hoc/:slug"
+            element={<AnnouncementDetailPage />}
+          />
+          <Route
+            path="/thong-bao/tuyen-sinh-dai-hoc"
+            element={<AnnouncementsUniversityPage />}
+          />
+          <Route
+            path="/thong-bao/tuyen-sinh-sau-dai-hoc"
+            element={<AnnouncementsPostgraduatePage />}
+          />
+          <Route
+            path="/thong-bao/tuyen-sinh-khac"
+            element={<AnnouncementsOtherPage />}
+          />
+          <Route
+            path="/tin-tuc/thong-tin-bao-chi/:slug"
+            element={<NewsDetailPage />}
+          />
+          <Route
+            path="/tin-tuc/thong-tin-bao-chi"
+            element={<NewsPressPage />}
+          />
+          <Route
+            path="/tin-tuc/hoat-dong-su-kien"
+            element={<NewsEventsPage />}
+          />
+          <Route path="/tra-cuu-tuyen-sinh" element={<AdmissionLookupPage />} />
+          <Route path="/admin/login" element={<AdminLoginPage />} />
+          <Route path="/admin/dashboard" element={<AdminDashboardPage />} />
+        </Routes>
+      </main>
+      {!isAdminRoute && <Footer />}
+      {!isAdminRoute && <ScrollToTopButton />}
+    </div>
+  );
+};
 
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route
-              path="/thong-bao/tuyen-sinh-dai-hoc/:slug"
-              element={<AnnouncementDetailPage />}
-            />
-            <Route
-              path="/thong-bao/tuyen-sinh-dai-hoc"
-              element={<AnnouncementsUniversityPage />}
-            />
-            <Route
-              path="/thong-bao/tuyen-sinh-sau-dai-hoc"
-              element={<AnnouncementsPostgraduatePage />}
-            />
-            <Route
-              path="/thong-bao/tuyen-sinh-khac"
-              element={<AnnouncementsOtherPage />}
-            />
-            <Route
-              path="/tin-tuc/thong-tin-bao-chi/:slug"
-              element={<NewsDetailPage />}
-            />
-            <Route
-              path="/tin-tuc/thong-tin-bao-chi"
-              element={<NewsPressPage />}
-            />
-            <Route
-              path="/tin-tuc/hoat-dong-su-kien"
-              element={<NewsEventsPage />}
-            />
-            <Route
-              path="/tra-cuu-tuyen-sinh"
-              element={<AdmissionLookupPage />}
-            />
-          </Routes>
-        </main>
-        <Footer />
-        <ScrollToTopButton />
-      </div>
+      <AppLayout />
     </Router>
   );
 }
---
+++ src/pages/AdminDashboardPage.js
@@ -0,0 +1,9 @@
+const AdminDashboardPage = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Trang quản trị</h1>
+      <p>Chào mừng đến với trang quản lý.</p>
+    </div>
+  );
+};
+export default AdminDashboardPage;
