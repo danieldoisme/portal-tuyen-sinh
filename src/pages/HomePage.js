bc88ee2afde068a463e1203d54a6efbe4906c6b1
danielthanh <danieldoisme@gmail.com>
2025-07-25 08:17:08 +0700

    Fix Homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,11 +3,24 @@ import Announcements from "../features/announcements/Announcements";
 import Banner from "../components/layout/Banner";
 import CallToAction from "../components/ui/CallToAction";
 
+import PTITRecruitmentSection from "../components/PTITRecruitmentSection";
+import AdmissionProcess from "../components/AdmissionProcess";
+import AdmissionTimeline from "../components/AdmissionTimeline";
+import RegulationsSlider from "../components/RegulationsSlider";
+import AdmissionMethods from "../components/AdmissionMethods";
+import FAQSection from "../components/FAQSection";
+
 const HomePage = () => {
   return (
     <>
       <Banner />
       <Admissions />
+      <PTITRecruitmentSection />
+      <AdmissionProcess />
+      <AdmissionTimeline />
+      <RegulationsSlider />
+      <AdmissionMethods />
+      <FAQSection />
       <Announcements />
       <CallToAction />
     </>
